274e9f0bc50b97ec29d1960422336b2c371103ca
SFRL <sebastian.lobbers@googlemail.com>
2024-12-05 12:15:13 +0000

    fix: update routing to use HashRouter in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState, useEffect } from 'react';
-import { Route, Routes} from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/home';
 import Dashboard from './pages/dashboard';
 import DiagramNavigation from "./pages/diagramNavigation";
