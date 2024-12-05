3579c3f42964c9862345600af3a2e7d66ae33eb1
SFRL <sebastian.lobbers@googlemail.com>
2024-12-05 11:30:32 +0000

    add config for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true, // Allows access from the local network
-    port: 5173, // Optional: Specify a custom port
-  },
+export default defineConfig(({ command, mode }) =>{
+
+    return {
+      plugins: [react()],
+      base: mode === "development" ? "/" : "/adsolve_evaluation_platform/",
+      server: {
+        host: true, // Allows access from the local network
+        port: 5173, // Optional: Specify a custom port
+    },
+  }
 });
