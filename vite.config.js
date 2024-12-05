d1978350b3c64334b32b6c102513188cd7b39a67
SFRL <sebastian.lobbers@googlemail.com>
2024-12-05 12:57:59 +0000

    fix: update base path in vite.config.js for production environment

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) =>{
+export default defineConfig(() =>{
 
     return {
       plugins: [react()],
-      base: mode === "development" ? "/" : "/adsolve_evaluation_platform/",
+      base: "/", // Base for production
       server: {
         host: true, // Allows access from the local network
         port: 5173, // Optional: Specify a custom port
