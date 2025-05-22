9521e5beb2a9d5c48f296bf6f4e2b7ba327463ee
SFRL <sebastian.lobbers@googlemail.com>
2025-05-22 16:05:12 +0000

    add: implement MHIC and EA metrics for evaluating summaries

---
+++ evaluation_bundles/metrics/mhic.py
@@ -0,0 +1,27 @@
+import bert_score
+from nltk import sent_tokenize
+import numpy as np
+
+
+
+class MHIC:
+    def __init__(self):
+        self.bs_scorer = bert_score.BERTScorer(
+            model_type="microsoft/deberta-xlarge-mnli",
+            device="cuda",
+            lang="en"
+        ) 
+
+    def calculate_metric(self, candidate, references):
+        results = []
+        candidate_sentences = sent_tokenize(candidate)
+        
+        for reference in references: 
+            _, R, _ = self.bs_scorer.score(
+                cands=candidate_sentences,
+                refs=[reference]*len(candidate_sentences)
+            )
+
+            results.append(R.max().item())
+        
+        return np.mean(results)
