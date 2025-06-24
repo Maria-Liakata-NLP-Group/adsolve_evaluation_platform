bb7e7b683062c03f83785c588ff135d2e9f08d5c
SFRL <sebastian.lobbers@googlemail.com>
2025-06-24 23:55:13 +0000

    Add evaluation functionality for court case and social media summarisation

---
+++ evaluation_bundles/court_case_summarisation_evaluation.py
@@ -0,0 +1,76 @@
+import argparse
+import sys
+import os
+import json
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from metrics.intra_nli import IntraNLI
+from metrics.readability import ReadabilityMetric
+
+
+class CourtCaseSummarisationEvaluationBundle:
+    def __init__(self):
+        self.readability_metric = ReadabilityMetric("flesch_kincaid")
+        self.intra_nli = IntraNLI()
+
+    def evaluate(self, llm_summaries: dict, gold_summaries: dict) -> dict:
+        # Dictionary to store results
+        results = {
+            'readability': {
+                "document_level": [],
+                "mean": None,
+            },
+            'intra_nli': {
+                "document_level": [],
+                "mean": None,
+            },
+            'document_ids': list(llm_summaries.keys())
+        }
+
+        # iterate over each document
+        for document_id in results['document_ids']:
+            llm_summary = llm_summaries[document_id]
+            gold_summary = gold_summaries[document_id]
+
+            # Calculate readability score
+            readability_score = self.readability_metric.calculate_metric(llm_summary)
+            results['readability']['document_level'].append(readability_score)
+
+            # Calculate intra-NLI score
+            intra_nli_score = self.intra_nli.calculate_metric(llm_summary)
+            results['intra_nli']['document_level'].append(intra_nli_score)
+
+        # Calculate mean scores
+        results['readability']['mean'] = sum(results['readability']['document_level']) / len(results['readability']['document_level'])
+        results['intra_nli']['mean'] = sum(results['intra_nli']['document_level']) / len(results['intra_nli']['document_level'])
+
+        return results
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluate court case summarisation.")
+    parser.add_argument('--llm_summaries', type=str, required=True, help=' Path to the LLM summaries JSON file.')
+    parser.add_argument('--gold_summaries', type=str, required=True, help='Path to the gold summaries JSON file.')
+    parser.add_argument('--output_file', type=str, default='court_case_summarisation_evaluation_results.json', help='Path to save the evaluation results JSON file.')
+    args = parser.parse_args()
+
+    # Load LLM summaries
+    print(f"Loading LLM summaries from {args.llm_summaries}")
+    with open(args.llm_summaries, 'r') as f:
+        llm_summaries = json.load(f)    
+    # Load gold summaries
+    print(f"Loading gold summaries from {args.gold_summaries}")
+    with open(args.gold_summaries, 'r') as f:
+        gold_summaries = json.load(f)   
+    # Create evaluation bundle
+    print("Creating evaluation bundle for court case summarisation.")
+    evaluation_bundle = CourtCaseSummarisationEvaluationBundle()
+    # Evaluate
+    print("Evaluating LLM.")
+    results = evaluation_bundle.evaluate(llm_summaries, gold_summaries)
+    
+    # Save results
+    print(f"Saving evaluation results to {args.output_file}")
+    output_file = args.output_file
+    with open(output_file, 'w') as f:
+        json.dump(results, f, indent=4)
--- evaluation_bundles/social_media_summarisation_evaluation.py
+++ evaluation_bundles/social_media_summarisation_evaluation.py
@@ -1,6 +1,8 @@
 # add metrics folder to paths from which to import classes
+import argparse
 import sys
 import os
+import json
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/metrics")
 
 from mhic import MHIC
@@ -21,70 +23,106 @@ class SocialMediaSummarisationEvaluationBundle:
         self.bert_score = BERTScore()
 
     def evaluate(self, posts: dict, llm_summaries: dict, gold_summaries: dict) -> dict:
-        # extract timeline ids from posts dict
-        timeline_ids = list(posts.keys())
+        # extract document ids from posts dict
+        document_ids = list(posts.keys())
 
         # Dictionary to store results
         results = {
             'mhic': {
-                "timeline_level": [],
+                "document_level": [],
                 "mean": None,
             },
             'intra_nli': {
-                "timeline_level": [],
+                "document_level": [],
                 "mean": None,
             },
             'fc_expert': {
-                "timeline_level": [],
+                "document_level": [],
                 "mean": None,
             },
-            'fc_timeline': {
-                "timeline_level": [],
+            'fc_document': {
+                "document_level": [],
                 "mean": None,
             },
             'style_similarity': {
-                "timeline_level": [],
+                "document_level": [],
                 "mean": None,
             },
             'bert_score': {
-                "timeline_level": [],
+                "document_level": [],
                 "mean": None,
             },
-            'timeline_ids': timeline_ids
+            'document_ids': document_ids
         }
 
-        # iterate over each timeline
-        for timeline_id in timeline_ids:
-            timeline_posts = posts[timeline_id]
-            llm_summary = llm_summaries[timeline_id]
-            gold_summary = gold_summaries[timeline_id]
+        # iterate over each document
+        for document_id in document_ids:
+            document_posts = posts[document_id]
+            llm_summary = llm_summaries[document_id]
+            gold_summary = gold_summaries[document_id]
 
             # Evaluate MHIC
-            mhic_score = self.mhic.calculate_metric(llm_summary, timeline_posts)
-            results['mhic']['timeline_level'].append(mhic_score)
+            mhic_score = self.mhic.calculate_metric(llm_summary, document_posts)
+            results['mhic']['document_level'].append(mhic_score)
 
             # Evaluate IntraNLI
             intra_nli_score = self.intra_nli.calculate_metric(llm_summary)
-            results['intra_nli']['timeline_level'].append(intra_nli_score)
+            results['intra_nli']['document_level'].append(intra_nli_score)
 
             # Evaluate FCExpert
             fc_expert_score = self.fc.calculate_metric(llm_summary, gold_summary)
-            results['fc_expert']['timeline_level'].append(fc_expert_score)
+            results['fc_expert']['document_level'].append(fc_expert_score)
 
-            # Evaluate FCTimeline 
-            fc_timeline_score = self.fc.calculate_metric(llm_summary, timeline_posts)
-            results['fc_timeline']['timeline_level'].append(fc_timeline_score)
+            # Evaluate FCdocument 
+            fc_document_score = self.fc.calculate_metric(llm_summary, document_posts)
+            results['fc_document']['document_level'].append(fc_document_score)
 
             # Evaluate Style Similarity
             style_similarity_score = self.style_similarity.calculate_metric(llm_summary, gold_summary)
-            results['style_similarity']['timeline_level'].append(style_similarity_score)
+            results['style_similarity']['document_level'].append(style_similarity_score)
 
             # Evaluate BERT Score
             bert_score = self.bert_score.calculate_metric(llm_summary, gold_summary)
-            results['bert_score']['timeline_level'].append(bert_score)
+            results['bert_score']['document_level'].append(bert_score)
 
 
-        for metric in ['mhic', 'intra_nli', 'fc_expert', 'fc_timeline', 'style_similarity', 'bert_score']:
+        for metric in ['mhic', 'intra_nli', 'fc_expert', 'fc_document', 'style_similarity', 'bert_score']:
             # Calculate mean for each metric
-            results[metric]['mean'] = np.mean(results[metric]['timeline_level'])
+            results[metric]['mean'] = np.mean(results[metric]['document_level'])
         return results
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluate social media summarisation.")
+    parser.add_argument('--posts', type=str, required=True, help='Path to the posts JSON file.')
+    parser.add_argument('--llm_summaries', type=str, required=True, help='Path to the LLM summaries JSON file.')
+    parser.add_argument('--gold_summaries', type=str, required=True, help='Path to the gold summaries JSON file.')
+    parser.add_argument('--output_file', type=str, default='social_media_summarisation_evaluation_results.json', help='Path to save the evaluation results JSON file.')
+    args = parser.parse_args()
+
+    # Load posts
+    print(f"Loading posts from {args.posts}")
+    with open(args.posts, 'r') as f:
+        posts = json.load(f)
+
+    # Load LLM summaries
+    print(f"Loading LLM summaries from {args.llm_summaries}")
+    with open(args.llm_summaries, 'r') as f:
+        llm_summaries = json.load(f)
+
+    # Load gold summaries
+    print(f"Loading gold summaries from {args.gold_summaries}")
+    with open(args.gold_summaries, 'r') as f:
+        gold_summaries = json.load(f)
+
+    # Create evaluation bundle
+    print("Creating evaluation bundle for social media summarisation.")
+    evaluation_bundle = SocialMediaSummarisationEvaluationBundle()
+
+    # Evaluate
+    print("Evaluating LLM.")
+    results = evaluation_bundle.evaluate(posts, llm_summaries, gold_summaries)
+
+    # Save results
+    print(f"Saving evaluation results to {args.output_file}")
+    with open(args.output_file, 'w') as f:
+        json.dump(results, f, indent=4)
