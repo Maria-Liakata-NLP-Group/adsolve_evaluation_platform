621d8bbc7493d6564578d650a3d6240df22eb4c7
SFRL <sebastian.lobbers@googlemail.com>
2026-05-15 14:11:28 +0100

    feat: update routes to use pathId for dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 /** @format */
 
-// import { useState, useEffect } from 'react';
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Dashboard from "./pages/dashboard";
@@ -12,40 +11,19 @@ import CreateNew from "./pages/createNew";
 import "./style.scss";
 
 const App = () => (
-	<div className="is-flex is-justify-content-center">
-		<div style={{ width: "100%", maxWidth: "1400px" }}>
-			<Routes>
-				<Route
-					path="/"
-					element={<Home />}
-				/>
-				<Route
-					path="/intrinsic-metrics"
-					element={<IntrinsicMetrics />}
-				/>
-				<Route
-					path="/use-cases"
-					element={<UseCases />}
-				/>
-				<Route
-					path="/use-cases/:useCase"
-					element={<UseCaseExamples />}
-				/>
-				<Route
-					path="/diagram"
-					element={<DiagramNavigation />}
-				/>
-				<Route
-					path="/use-cases/:useCase/:task"
-					element={<Dashboard />}
-				/>
-				<Route
-					path="/evaluation-script-builder"
-					element={<CreateNew />}
-				/>
-			</Routes>
-		</div>
-	</div>
+  <div className="is-flex is-justify-content-center">
+    <div style={{ width: "100%", maxWidth: "1400px" }}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/intrinsic-metrics" element={<IntrinsicMetrics />} />
+        <Route path="/use-cases" element={<UseCases />} />
+        <Route path="/use-cases/:useCaseId" element={<UseCaseExamples />} />
+        <Route path="/use-cases/tasks/:pathId" element={<Dashboard />} />
+        <Route path="/diagram" element={<DiagramNavigation />} />
+        <Route path="/evaluation-script-builder" element={<CreateNew />} />
+      </Routes>
+    </div>
+  </div>
 );
 
 export default App;
