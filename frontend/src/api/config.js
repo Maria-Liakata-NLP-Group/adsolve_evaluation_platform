eb6e672d9b3a32bb1ae12dc9ca0876a8908a9701
SFRL <sebastian.lobbers@googlemail.com>
2026-05-15 14:05:36 +0100

    feat: add config service functions and usePathConfig hook

---
+++ frontend/src/api/config.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import { get } from './client';
+
+export const getUseCases = () => get('/api/use-cases');
+
+export const getPaths = (useCaseId) =>
+  get(useCaseId ? `/api/paths?use_case_id=${useCaseId}` : '/api/paths');
+
+export const getPath = (pathId) => get(`/api/paths/${pathId}`);
+
+export const getInfrastructure = () => get('/api/infrastructure');
