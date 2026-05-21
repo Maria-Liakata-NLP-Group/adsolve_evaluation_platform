84da4166c4731c88c31dc2ae3bf40338b39d0fe1
SFRL <sebastian.lobbers@googlemail.com>
2026-05-21 16:42:28 +0100

    feat: add AppHeader to interior routes, fix layout wrapper

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 /** @format */
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import AppHeader from "./components/AppHeader";
 import Home from "./pages/home";
 import Dashboard from "./pages/dashboard";
 import UseCases from "./pages/useCases";
@@ -9,37 +10,25 @@ import IntrinsicMetrics from "./pages/intrinsicMetrics";
 import CreateNew from "./pages/createNew";
 import "./style.scss";
 
-const App = () => (
-	<div className="is-flex is-justify-content-center">
-		<div style={{ width: "100%", maxWidth: "1400px" }}>
-			<Routes>
-				<Route
-					path="/"
-					element={<Home />}
-				/>
-				<Route
-					path="/intrinsic-metrics"
-					element={<IntrinsicMetrics />}
-				/>
-				<Route
-					path="/use-cases"
-					element={<UseCases />}
-				/>
-				<Route
-					path="/use-cases/:useCaseId"
-					element={<Tasks />}
-				/>
-				<Route
-					path="/use-cases/:useCaseId/:pathId"
-					element={<Dashboard />}
-				/>
-				<Route
-					path="/evaluation-script-builder"
-					element={<CreateNew />}
-				/>
-			</Routes>
-		</div>
-	</div>
-);
+const App = () => {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
+  return (
+    <>
+      {!isHome && <AppHeader />}
+      <div style={!isHome ? { maxWidth: "1400px", margin: "0 auto", width: "100%", padding: "0 2rem" } : {}}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/intrinsic-metrics" element={<IntrinsicMetrics />} />
+          <Route path="/use-cases" element={<UseCases />} />
+          <Route path="/use-cases/:useCaseId" element={<Tasks />} />
+          <Route path="/use-cases/:useCaseId/:pathId" element={<Dashboard />} />
+          <Route path="/evaluation-script-builder" element={<CreateNew />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
 
 export default App;
